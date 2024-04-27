7a48725b6c8d9917edf7e532ff3d17329145850d
karasb1 <bohdankaras456@gmail.com>
2024-04-27 16:59:52 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main()
 {
-
+	int a;
+	cin >> a;
+	(a / 1000 < 0 || a / 1000 > 10) ? cout << "Enter a four-digit number" : cout << (a%1000/100*1000)+(a/1000*100)+(a%100/10)+(a%10*10);
 
 	return 0;
 }
