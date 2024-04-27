5064e0b1c56dcd6dcc38e306dbedbed2bc13532f
karasb1 <bohdankaras456@gmail.com>
2024-04-27 16:53:55 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
+	int a;
+	cin >> a;
+	(a / 100000 < 0 || a / 100000 > 10) ? cout << "Enter a six-digit number" : (((a / 100000) + (a % 100000 / 10000) + (a % 10000 / 1000)) == ((a % 1000 / 100) + (a % 100 / 10) + (a % 10))) ? cout << "It`s a six-digit number" : cout << "This is not a six-digit number";
+	
 
 
 	return 0;
