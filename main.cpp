01e87f211ed4c0fbd0b9bd3eac5011ea1cdcc753
karasb1 <bohdankaras456@gmail.com>
2024-04-27 17:05:23 +0300

    The ex3 was done.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,32 @@ using namespace std;
 
 int main()
 {
-
-
+	int a, b, c, d, e, f, g;
+	cin >> a;
+	cin >> b;
+	if (a < b) {
+		a = b;
+	}
+	cin >> c;
+	if (a < c) {
+		a = c;
+	}
+	cin >> d;
+	if (a < d) {
+		a = d;
+	}
+	cin >> e;
+	if (a < e) {
+		a = e;
+	}
+	cin >> f;
+	if (a < f) {
+		a = f;
+	}
+	cin >> g;
+	if (a < g) {
+		a = g;
+	}
+	cout << a;
 	return 0;
 }
