966ad65f4932a8c150edb28b678a32a5be585bb7
karasb1 <bohdankaras456@gmail.com>
2024-04-27 16:09:14 +0300

    The ex4 was done.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main()
 {
-
+	double a, b;
+	cin >> a;
+	cin >> b;
+	(a == b) ? cout << a << " == " << b : (a > b) ? cout << b << " " << a : cout << a << " " << b;
 
 	return 0;
 }
